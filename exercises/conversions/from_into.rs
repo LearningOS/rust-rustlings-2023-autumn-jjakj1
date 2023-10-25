74ea615340d5e5525a41a21c21a592abaf86677f
Ares Li <aresli96@outlook.com>
2023-10-25 17:25:11 +0800

    finish all

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,25 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            return Person::default();
+        }
+
+        let person_info: Vec<&str> = s.split(',').collect();
+        if person_info.len() != 2 {
+            return Person::default();
+        }
+        let name = person_info[0].to_owned();
+        if name.is_empty() {
+            return Person::default();
+        }
+        if let Ok(age) = person_info[1].parse::<usize>() {
+            Person { name, age }
+        } else {
+            Person::default()
+        }
     }
 }
 
